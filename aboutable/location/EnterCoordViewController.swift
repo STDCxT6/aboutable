cc27be51f90051a74df21e0905ea9da1847dd15f
masma-dev <masma.work@gmail.com>
2024-05-17 12:05:30 +0800

    Update EnterCoordViewController.swift

--- aboutable/location/EnterCoordViewController.swift
+++ aboutable/location/EnterCoordViewController.swift
@@ -12,9 +12,10 @@ class EnterCoordViewController: UIViewController {
 
     @IBOutlet var latitudeTextField: UITextField!
     @IBOutlet var longitudeTextField: UITextField!
-//    var latituteText: String?
-//    var longitudeText: String?
     var newCoordinate: CLLocationCoordinate2D?
+    
+    /// Use to save data to local storage
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +23,20 @@ class EnterCoordViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        latitudeTextField.text = defaults.object(forKey: "latitude") as? String
+        longitudeTextField.text = defaults.object(forKey: "longitude") as? String
+        
+    }
+    
 //     MARK: - Navigation
 
 //     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let latituteText = latitudeTextField, let longitudeText = longitudeTextField, let latitude = Double(latitudeTextField.text!), let longitude = Double(longitudeTextField.text!) {
+        if let latitudeText = latitudeTextField, let longitudeText = longitudeTextField, let latitude = Double(latitudeTextField.text!), let longitude = Double(longitudeTextField.text!) {
+            defaults.setValue(latitudeText.text, forKey: "latitude")
+            defaults.setValue(longitudeText.text, forKey: "longitude")
             newCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         }
     }
